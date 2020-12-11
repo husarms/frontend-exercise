748a6e08926c6c23c053454fcb189a33dcf05a87
Matthew-S-Husar <Matthew_S_Husar@carmax.com>
2020-12-11 01:56:03 -0500

    Update date-utils.js

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -1,8 +1,9 @@
 export function localizeDate(dateString) {
     const date = new Date(dateString);
-    return date.toLocaleDateString(undefined, {
+    return date.toLocaleDateString("en-US", {
         month: "long",
         day: "numeric",
         year: "numeric",
+        timeZone: 'UTC',
     });
 }
